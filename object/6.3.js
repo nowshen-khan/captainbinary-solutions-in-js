84059e4b1067f80764197ad5814c938ad5fe9078
Nowshen Anjuman Khan Prapte <nowshen.anjum@gmail.com>
2024-10-07 22:58:33 +0600

    object

--- object/6.3.js
+++ object/6.3.js
@@ -1,5 +1,6 @@
 //একটি অবজেক্টে নতুন ( কী-ভেলু ) জোড়া যুক্ত করো
 function addObjectProperty(object, key, value) {
 	// Write Your Code Here
-	return (object[key] = value);
-}
+	object[key]=value;
+	return object;
+  }
